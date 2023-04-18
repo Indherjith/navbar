2dda6f83f5581144827fdc39c8894c1b0a5d9f21
Buvaneswaran S <99597201+Indherjith@users.noreply.github.com>
2023-04-18 09:22:17 +0530

    main

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import "./Navbar.css"
+import { Link } from 'react-router-dom'
+import Show from './Show'
+import Hide from './Hide'
+
+const Navbar = () => {
+
+    const [show,setShow] = useState(true)
+
+  return (
+    <div className='nav'>
+        <div className="logo">
+            <img src="https://th.bing.com/th/id/OIP.s6RuZ2Cv4DtF-HuEFALqKwHaHa?w=164&h=180&c=7&r=0&o=5&pid=1.7" alt="" />
+        </div>
+        <div className="normal">
+            <Link to={"/"} className='Link'>Home</Link>
+            <Link to={"/about"} className='Link'>About</Link>
+            <Link to={"/contact"} className='Link'>Contact</Link>
+        </div>
+        <div className="user">
+            <div className="username">Buvaneswaran S</div>
+        </div>
+        {show?<button className="menu" onClick={()=>setShow(!show)}><img src="https://openclipart.org/image/800px/321014" alt="" /></button>:
+        <div className="small">
+            <button className="close" onClick={()=>setShow(!show)}><img src="https://th.bing.com/th/id/OIP.MvR8H4voNgN7Ej9WWRO8tgHaHa?w=179&h=180&c=7&r=0&o=5&pid=1.7" alt="" /></button>
+            <div className="username1">Buvaneswaran S</div>
+            <div className="Links">
+                <Link to={"/"} className='Link'>Home</Link>
+                <Link to={"/about"} className='Link'>About</Link>
+                <Link to={"/contact"} className='Link'>Contact</Link>
+            </div>
+        </div>
+        }
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Component/Show.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Show = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Show
